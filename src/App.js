e34787ff6e1230b908116d73c61a302b860151da
deependra8888 <deepender8888@gmail.com>
2026-05-02 18:22:20 +0530

    task done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { use, useState } from 'react';
 import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
+
+const users = [
+  { id: 1, name: "Rahul", status: "active" },
+  { id: 2, name: "Anita", status: "inactive" },
+  { id: 3, name: "Vikram", status: "active" },
+  { id: 4, name: "Sneha", status: "inactive" },
+  { id: 5, name: "Aman", status: "active" },
+];
 
 function App() {
-  return (
-    <Router>
+  const [userList, setUserList] = useState(users)
+  const [filter, setFilter] = useState("all")
+  const handleSearch = (e) => {
 
-      <div className="App">
-        
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
+    let searchVal = e.target.value.toLowerCase();
+    let searchedList = userList.filter(user => {
+      let userName = user.name.toLowerCase();
+      // console.log(userName, searchVal)
+      return userName.includes(searchVal)
+    })
+    // console.log(searchedList)
+
+    console.log(searchedList)
+    setUserList(searchedList)
+  }
+  const handleFilter = () => { }  // console.log(userList)
+  return (
+    <>
+      <div >
+        <input onChange={(e) => handleSearch(e)} />
+        <select id="statusFilter" onchange="filterData()">
+          <option value="all">All</option>
+          <option value="active">Active</option>
+          <option value="inactive">Inactive</option>
+        </select>
+        {userList.map(user => {
+          return <div style={{ display: 'flex', gap: '1rem' }} >
+            <span >{user.name}</span>
+            <span>{user.status}</span>
+          </div>
+        })}
       </div>
-    </Router>
+    </>
   );
 }
 
